5ea009f8aa71a1d59638569df7b961df59de3e3c
Justin <justin@clowd.io>
2024-12-20 18:41:18 -0700

    Disable keyboard navigation on ATIS preset combobox

---
+++ vATIS.Desktop/Ui/Controls/NonNavigableComboBox.cs
@@ -0,0 +1,20 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace Vatsim.Vatis.Ui.Controls;
+
+public class NonNavigableComboBox : ComboBox
+{
+    protected override Type StyleKeyOverride => typeof(ComboBox);
+    
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        if (e.Key is Key.Up or Key.Down)
+        {
+            // Disable up/down arrow navigation
+            e.Handled = true;
+        }
+        base.OnKeyDown(e);
+    }
+}
