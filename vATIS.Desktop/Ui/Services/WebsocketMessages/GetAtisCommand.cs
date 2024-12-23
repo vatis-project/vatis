d7ade8123b5613992248658d72491a3a84b82dd5
Neil Enns <neile@live.com>
2024-12-23 10:22:03 -0800

    More comments

--- vATIS.Desktop/Ui/Services/WebsocketMessages/GetAtisCommand.cs
+++ vATIS.Desktop/Ui/Services/WebsocketMessages/GetAtisCommand.cs
@@ -1,7 +1,17 @@
 namespace Vatsim.Vatis.Ui.Services.WebsocketMessages;
 
+/// <summary>
+/// Represents a message to get the ATIS for a specific station.
+/// </summary>
 class GetAtisMessage
 {
+	/// <summary>
+	/// Gets or sets the key identifying the message as a get ATIS message.
+	/// </summary>
 	public string? Key { get; set; }
+
+	/// <summary>
+	/// Gets or sets the station to get the ATIS for.
+	/// </summary>
 	public string? Station { get; set; }
 }
--- vATIS.Desktop/Ui/Services/WebsocketMessages/MessageBase.cs
+++ vATIS.Desktop/Ui/Services/WebsocketMessages/MessageBase.cs
@@ -2,9 +2,18 @@ using System.Text.Json.Nodes;
 
 namespace Vatsim.Vatis.Ui.Services.WebsocketMessages;
 
+/// <summary>
+/// Represents a message sent over the websocket.
+/// </summary>
 public class MessageBase
 {
+  /// <summary>
+  /// Gets or sets the key identifying the message.
+  /// </summary>
   public string Key { get; set; } = string.Empty;
 
+  /// <summary>
+  /// Gets or sets the value of the message.
+  /// </summary>
   public JsonValue? Value { get; set; }
 }
