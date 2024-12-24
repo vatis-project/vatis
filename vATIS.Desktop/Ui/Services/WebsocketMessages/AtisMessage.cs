ec1390b6e17c04d81c9e861a9a1ad3bea93fc865
Neil Enns <neile@live.com>
2024-12-24 09:41:00 -0800

    Don't send null values

--- vATIS.Desktop/Ui/Services/WebsocketMessages/AtisMessage.cs
+++ vATIS.Desktop/Ui/Services/WebsocketMessages/AtisMessage.cs
@@ -19,54 +19,63 @@ public class AtisMessage()
         /// </summary>
         [JsonPropertyName("networkConnectionStatus")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public NetworkConnectionStatus? NetworkConnectionStatus { get; set; }
 
         /// <summary>
         /// Gets the ATIS message text.
         /// </summary>
         [JsonPropertyName("textAtis")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? TextAtis { get; set; }
 
         /// <summary>
         /// Gets the station ID of the ATIS message.
         /// </summary>
         [JsonPropertyName("station")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Station { get; set; }
 
         /// <summary>
         /// Gets the type of the ATIS message.
         /// </summary>
         [JsonPropertyName("atisType")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public AtisType? AtisType { get; set; }
 
         /// <summary>
         /// Gets the ATIS letter.
         /// </summary>
         [JsonPropertyName("atisLetter")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public char? AtisLetter { get; set; }
 
         /// <summary>
         /// Gets the METAR used to create the ATIS.
         /// </summary>
         [JsonPropertyName("metar")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Metar { get; set; }
 
         /// <summary>
         /// Gets the current winds.
         /// </summary>
         [JsonPropertyName("wind")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Wind { get; set; }
 
         /// <summary>
         /// Gets the current altimeter.
         /// </summary>
         [JsonPropertyName("altimeter")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Altimeter { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether the ATIS message is new.
         /// </summary>
         [JsonPropertyName("isNewAtis")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public bool? IsNewAtis { get; set; }
     }
 
