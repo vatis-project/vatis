4524e29983d1612c521f61295a6a37c596c38985
Neil <neile@live.com>
2024-12-24 07:42:13 -0800

    More indentation fixes

--- vATIS.Desktop/Ui/Services/WebsocketMessages/AtisMessage.cs
+++ vATIS.Desktop/Ui/Services/WebsocketMessages/AtisMessage.cs
@@ -9,76 +9,76 @@ namespace Vatsim.Vatis.Ui.Services.WebsocketMessages;
 /// </summary>
 public class AtisMessage()
 {
-  /// <summary>
-  /// Represents the value of an ATIS message.
-  /// </summary>
-  public class AtisMessageValue(string stationId, AtisType atisType, char atisLetter, string? metar, string? wind, string? altimeter, string? textAtis, bool isNewAtis, NetworkConnectionStatus networkConnectionStatus)
-  {
     /// <summary>
-    /// Gets the network connection status.
+    /// Represents the value of an ATIS message.
     /// </summary>
-    [JsonPropertyName("networkConnectionStatus")]
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public NetworkConnectionStatus NetworkConnectionStatus { get; } = networkConnectionStatus;
+    public class AtisMessageValue(string stationId, AtisType atisType, char atisLetter, string? metar, string? wind, string? altimeter, string? textAtis, bool isNewAtis, NetworkConnectionStatus networkConnectionStatus)
+    {
+        /// <summary>
+        /// Gets the network connection status.
+        /// </summary>
+        [JsonPropertyName("networkConnectionStatus")]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public NetworkConnectionStatus NetworkConnectionStatus { get; } = networkConnectionStatus;
+
+        /// <summary>
+        /// Gets the ATIS message text.
+        /// </summary>
+        [JsonPropertyName("textAtis")]
+        public string? TextAtis { get; } = textAtis;
+
+        /// <summary>
+        /// Gets the station ID of the ATIS message.
+        /// </summary>
+        [JsonPropertyName("stationId")]
+        public string StationId { get; } = stationId;
+
+        /// <summary>
+        /// Gets the type of the ATIS message.
+        /// </summary>
+        [JsonPropertyName("atisType")]
+        public AtisType AtisType { get; } = atisType;
+
+        /// <summary>
+        /// Gets the ATIS letter.
+        /// </summary>
+        [JsonPropertyName("atisLetter")]
+        public char AtisLetter { get; } = atisLetter;
+
+        /// <summary>
+        /// Gets the METAR used to create the ATIS.
+        /// </summary>
+        [JsonPropertyName("metar")]
+        public string? Metar { get; } = metar;
+
+        /// <summary>
+        /// Gets the current winds.
+        /// </summary>
+        [JsonPropertyName("wind")]
+        public string? Wind { get; } = wind;
+
+        /// <summary>
+        /// Gets the current altimeter.
+        /// </summary>
+        [JsonPropertyName("altimeter")]
+        public string? Altimeter { get; } = altimeter;
+
+        /// <summary>
+        /// Gets a value indicating whether the ATIS message is new.
+        /// </summary>
+        [JsonPropertyName("isNewAtis")]
+        public bool IsNewAtis { get; } = isNewAtis;
+    }
 
     /// <summary>
-    /// Gets the ATIS message text.
+    /// Gets the string identifying the message as an ATIS message.
     /// </summary>
-    [JsonPropertyName("textAtis")]
-    public string? TextAtis { get; } = textAtis;
+    [JsonPropertyName("type")]
+    public string MessageType { get; } = "atis";
 
     /// <summary>
-    /// Gets the station ID of the ATIS message.
+    /// Gets or sets the ATIS information.
     /// </summary>
-    [JsonPropertyName("stationId")]
-    public string StationId { get; } = stationId;
-
-    /// <summary>
-    /// Gets the type of the ATIS message.
-    /// </summary>
-    [JsonPropertyName("atisType")]
-    public AtisType AtisType { get; } = atisType;
-
-    /// <summary>
-    /// Gets the ATIS letter.
-    /// </summary>
-    [JsonPropertyName("atisLetter")]
-    public char AtisLetter { get; } = atisLetter;
-
-    /// <summary>
-    /// Gets the METAR used to create the ATIS.
-    /// </summary>
-    [JsonPropertyName("metar")]
-    public string? Metar { get; } = metar;
-
-    /// <summary>
-    /// Gets the current winds.
-    /// </summary>
-    [JsonPropertyName("wind")]
-    public string? Wind { get; } = wind;
-
-    /// <summary>
-    /// Gets the current altimeter.
-    /// </summary>
-    [JsonPropertyName("altimeter")]
-    public string? Altimeter { get; } = altimeter;
-
-    /// <summary>
-    /// Gets a value indicating whether the ATIS message is new.
-    /// </summary>
-    [JsonPropertyName("isNewAtis")]
-    public bool IsNewAtis { get; } = isNewAtis;
-  }
-
-  /// <summary>
-  /// Gets the string identifying the message as an ATIS message.
-  /// </summary>
-  [JsonPropertyName("type")]
-  public string MessageType { get; } = "atis";
-
-  /// <summary>
-  /// Gets or sets the ATIS information.
-  /// </summary>
-  [JsonPropertyName("value")]
-  public AtisMessageValue? Value { get; set; }
+    [JsonPropertyName("value")]
+    public AtisMessageValue? Value { get; set; }
 }
--- vATIS.Desktop/Ui/Services/WebsocketMessages/ErrorMessage.cs
+++ vATIS.Desktop/Ui/Services/WebsocketMessages/ErrorMessage.cs
@@ -7,27 +7,27 @@ namespace Vatsim.Vatis.Ui.Services.WebsocketMessages;
 /// </summary>
 public class ErrorMessage
 {
-  /// <summary>
-  /// Represents the value of an error message.
-  /// </summary>
-  public class ErrorValue
-  {
     /// <summary>
-    /// Gets and sets the error message.
+    /// Represents the value of an error message.
     /// </summary>
-    [JsonPropertyName("message")]
-    public string Message { get; set; } = string.Empty;
-  }
+    public class ErrorValue
+    {
+        /// <summary>
+        /// Gets and sets the error message.
+        /// </summary>
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+    }
 
-  /// <summary>
-  /// Gets the key identifying the message as an error message.
-  /// </summary>
-  [JsonPropertyName("type")]
-  public string MessageType { get; } = "error";
+    /// <summary>
+    /// Gets the key identifying the message as an error message.
+    /// </summary>
+    [JsonPropertyName("type")]
+    public string MessageType { get; } = "error";
 
-  /// <summary>
-  /// Gets and sets the error information.
-  /// </summary>
-  [JsonPropertyName("value")]
-  public ErrorValue? Value { get; set; }
+    /// <summary>
+    /// Gets and sets the error information.
+    /// </summary>
+    [JsonPropertyName("value")]
+    public ErrorValue? Value { get; set; }
 }
