56d30e25de1a42d62d30b3481a9ba93bca15aa11
GeheimagentNr1 <geheimelache@hotmail.com>
2024-12-25 20:28:09 +0100

    VATIS-80: Fix appearing of * and {} in text ATIS

--- vATIS.Desktop/Atis/AtisBuilder.cs
+++ vATIS.Desktop/Atis/AtisBuilder.cs
@@ -246,8 +246,10 @@ public class AtisBuilder : IAtisBuilder
         template = Regex.Replace(template, @"\s+", " ");
         template = Regex.Replace(template, @"(?<=\*)(-?[\,0-9]+)", "$1");
         template = Regex.Replace(template, @"(?<=\#)(-?[\,0-9]+)", "$1");
+        template = Regex.Replace(template, @"\{(-?[\,0-9]+)\}", "$1");
         template = Regex.Replace(template, @"(?<=\+)([A-Z]{3})", "$1");
         template = Regex.Replace(template, @"(?<=\+)([A-Z]{4})", "$1");
+        template = Regex.Replace(template, @"\*", "");
         // strip caret from runway parsing
         template = Regex.Replace(template, @"(?<![\w\d])\^((?:0?[1-9]|[1-2][0-9]|3[0-6])(?:[LRC]?))(?![\w\d])", "$1");
 
