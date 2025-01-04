5d2d5459014049cc651825356e246999ffcd3f2f
Justin <justin@clowd.io>
2025-01-03 22:52:05 -0700

    Pass decoded METAR to ATIS builder

--- vATIS.Desktop/Atis/IAtisBuilder.cs
+++ vATIS.Desktop/Atis/IAtisBuilder.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Vatsim.Vatis.Profiles.Models;
+using Vatsim.Vatis.Weather.Decoder.Entity;
 
 namespace Vatsim.Vatis.Atis;
 
 public interface IAtisBuilder
 {
     Task<AtisBuilderResponse> BuildAtis(AtisStation station, AtisPreset preset, char currentAtisLetter,
-        object? stationMetar, CancellationToken cancellationToken, bool sandboxRequest = false);
+        DecodedMetar decodedMetar, CancellationToken cancellationToken, bool sandboxRequest = false);
     Task UpdateIds(AtisStation station, AtisPreset preset, char currentAtisLetter, CancellationToken cancellationToken);
 }
