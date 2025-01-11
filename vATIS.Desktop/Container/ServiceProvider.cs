192badd31f66ae6439f92dcf4ad970af1a30e7e5
Justin <justin@clowd.io>
2025-01-11 13:23:15 -0700

    Fix merge issues

--- vATIS.Desktop/Container/ServiceProvider.cs
+++ vATIS.Desktop/Container/ServiceProvider.cs
@@ -140,7 +140,7 @@ internal sealed partial class ServiceProvider
     {
         if (IsDevelopmentEnvironment())
         {
-            return new MockAtisHubConnection();
+            return new MockAtisHubConnection(GetService<IDownloader>(), GetService<IAppConfigurationProvider>());
         }
 
         return new AtisHubConnection(GetService<IAppConfigurationProvider>(), GetService<IClientAuth>());
--- vATIS.Desktop/Weather/Decoder/ChunkDecoder/CloudChunkDecoder.cs
+++ vATIS.Desktop/Weather/Decoder/ChunkDecoder/CloudChunkDecoder.cs
@@ -1,10 +1,11 @@
-// <copyright file="CloudChunkDecoder.cs" company="Afonso Dutra Nogueira Filho">
+﻿// <copyright file="CloudChunkDecoder.cs" company="Afonso Dutra Nogueira Filho">
 // Copyright (c) Afonso Dutra Nogueira Filho. All rights reserved.
 // Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
 // https://github.com/afonsoft/metar-decoder
 // </copyright>
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Vatsim.Vatis.Weather.Decoder.ChunkDecoder.Abstract;
 using Vatsim.Vatis.Weather.Decoder.Entity;
