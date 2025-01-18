504edc3ceb3230028c58ee8747e4b44f798729b4
Justin <justin@clowd.io>
2025-01-18 11:27:18 -0700

    Add missing license headers

--- vATIS.Desktop/Container/Factory/IVoiceServerConnectionFactory.cs
+++ vATIS.Desktop/Container/Factory/IVoiceServerConnectionFactory.cs
@@ -1,3 +1,8 @@
+// <copyright file="IVoiceServerConnectionFactory.cs" company="Justin Shannon">
+// Copyright (c) Justin Shannon. All rights reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
 using Vatsim.Vatis.Voice.Network;
 
 namespace Vatsim.Vatis.Container.Factory;
