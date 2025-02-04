356b29c6fbea681f25738bdb7b6b81a4ff9ca74b
Justin <justin@clowd.io>
2025-02-03 17:15:08 -0700

    Strip text parsing characters from text ATIS

--- vATIS.Desktop/Atis/AtisBuilder.cs
+++ vATIS.Desktop/Atis/AtisBuilder.cs
@@ -335,6 +335,8 @@ public class AtisBuilder : IAtisBuilder
             template += closingTemplate;
         }
 
+        template = RemoveTextParsingCharacters(template);
+
         return template;
     }
 
