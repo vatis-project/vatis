a1d86b68f47b4c71e170326fc365b5a1c676ee6d
Justin <justin@clowd.io>
2025-03-10 20:38:57 -0600

    Add missing license headers

--- vATIS.Desktop/Ui/Services/Websocket/WebsocketMessages/DisconnectAtisMessage.cs
+++ vATIS.Desktop/Ui/Services/Websocket/WebsocketMessages/DisconnectAtisMessage.cs
@@ -1,3 +1,8 @@
+// <copyright file="DisconnectAtisMessage.cs" company="Justin Shannon">
+// Copyright (c) Justin Shannon. All rights reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
 using System.Text.Json.Serialization;
 using Vatsim.Vatis.Profiles.Models;
 
