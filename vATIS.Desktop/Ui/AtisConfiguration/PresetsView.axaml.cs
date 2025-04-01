3a66e1b6d03c7d5d66c6f1a05f6ba8398b80c832
Justin <justin@clowd.io>
2025-04-01 17:01:26 -0600

    Remove unused TextChanged event handler

--- vATIS.Desktop/Ui/AtisConfiguration/PresetsView.axaml.cs
+++ vATIS.Desktop/Ui/AtisConfiguration/PresetsView.axaml.cs
@@ -29,17 +29,6 @@ public partial class PresetsView : UserControl
         InitializeComponent();
     }
 
-    private void AtisTemplate_OnTextChanged(object? sender, EventArgs e)
-    {
-        if (DataContext is PresetsViewModel model)
-        {
-            if (!AtisTemplate.TextArea.IsFocused)
-                return;
-
-            model.HasUnsavedChanges = ((AtisPreset)SelectedPreset.SelectedItem).Template != AtisTemplate.Text;
-        }
-    }
-
     private async void SelectedPreset_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         try
