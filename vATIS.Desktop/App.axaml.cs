cddc2b758b3c92bca60ea6b55deee6012b8c9dcd
Neil Enns <neile@live.com>
2025-04-03 06:13:20 -0700

    Remove unused using

--- vATIS.Desktop/App.axaml.cs
+++ vATIS.Desktop/App.axaml.cs
@@ -12,7 +12,6 @@ using System.Net;
 using System.Net.Http;
 using System.Reactive;
 using System.Reflection;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using AsyncAwaitBestPractices;
